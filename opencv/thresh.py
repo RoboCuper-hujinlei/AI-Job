08a55f8e2b76ab528a326ed1bd9485bbfbfb845b
HuJinlei <jinlei.hu@skysys.cn>
2021-11-15 20:53:07 +0800

    add thresh.py

---
+++ opencv/thresh.py
@@ -0,0 +1,54 @@
+'''
+    图像阈值操作
+'''
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+'''
+res, dst = cv2.threshold(src, thresh, maxval, type)
+
+thresh: 阈值，一般取127。
+maxval: 当像素值超过阈值(或小于阈值， 根据type来决定), 所赋予的值
+type: 二值化操作的类型， 包含以下5种类型：
+
+    cv2.THRESH_BINARY       
+    cv2.THRESH_BINARY_INV
+    cv2.THRESH_TRUNC
+    cv2.THRESH_TOZERO
+    cv2.THRESH_TOZERO_INV
+
+'''
+def cvshow(name, img):
+    cv2.imshow(name, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+img = cv2.imread('../data/cat.jpg')
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cvshow("gray", gray)
+
+ret, thresh1 = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)      # 二值法， 超过阈值thresh部分取最大值，否则取0
+ret, thresh2 = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)  # _INV(反转)，超过阈值thresh部分取0，否则取最大值
+ret, thresh3 = cv2.threshold(gray, 127, 255, cv2.THRESH_TRUNC)       # trunc 大于阈值部分取阈值(thresh)，否则不变
+ret, thresh4 = cv2.threshold(gray, 127, 255, cv2.THRESH_TOZERO)      # tozero 大于阈值部分不变，否则设为0
+ret, thresh5 = cv2.threshold(gray, 127, 255, cv2.THRESH_TOZERO_INV)  # _INV （反转）大于阈值部分设为0，否则不变
+
+print("ret", ret)   # 返回的阈值
+
+titles = ['original image', 'THRESH_BINARY', 'THRESH_BINARY_INV', 'THRESH_TRUNC', 'THRESH_TOZERO', 'THRESH_TOZERO_INV']
+images = [cv2.cvtColor(img, cv2.COLOR_BGR2RGB), thresh1, thresh2, thresh3, thresh4, thresh5]
+
+for i in range(6):
+    plt.subplot(2, 3, i+1),
+    plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([])
+    plt.yticks([])
+plt.show()
+
+
+
+
+
--- opencv/图像基本操作.py
+++ opencv/图像基本操作.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def cvshow(name, img):
     cv2.imshow(name, img)
-    cv2.waitKey(1000)
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 img = cv2.imread('../data/cat.jpg')
@@ -55,17 +55,79 @@ _b, _g, _r = img[:, :, 0], img[:, :, 1], img[:, :, 2]       # 法 2
 print(b == _b)
 
 
-img[:, :, 0] = 0
-img[:, :, 1] = 0
-cvshow('R', img)
+# 仅保留R通道
+copy_img[:, :, 0] = 0
+copy_img[:, :, 1] = 0
+cvshow('R', copy_img)
 
-
-            
-
+def padding():
+    '''
+        填充方法：
+        cv2.BORDER_REPLICATE：复制法，复制最边缘的像素填充
+        cv2.BORDER_REFLECT：反射法，对感兴趣的像素在两边进行复制，gfedcb|abcdefgh|gfedcba
+        cv2.BORDER_REFLECT_101：反射法，也就是以最边缘像素为轴对称
+        cv2.BORDER_WRAP：外包装法 cdefgh|abcdefgh|abcdefg
+        cv2.BORDER_CONSTANT：敞亮法，常数值填充
+    '''
     
+    # plt 用RGB格式显示，opencv用BGR格式显示
+    img = cv2.imread("../data/dog.jpg") 
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    top_size, bottom_size, left_size, right_size  = (50, 50, 50, 50)    # 指定上下左右填充的值
+    # 不同的填充方法, copy Make Border通过复制已有元素 制作边界 填充
+    replicate = cv2.copyMakeBorder(img,  top_size, bottom_size, left_size, right_size, borderType=cv2.BORDER_REPLICATE) # replicate复制
+    reflect = cv2.copyMakeBorder(img,  top_size, bottom_size, left_size, right_size, borderType=cv2.BORDER_REFLECT) # reflect 
+    reflect101 = cv2.copyMakeBorder(img,  top_size, bottom_size, left_size, right_size, borderType=cv2.BORDER_REFLECT_101) # reflect101 
+    wrap = cv2.copyMakeBorder(img, top_size, bottom_size, left_size, right_size, borderType=cv2.BORDER_WRAP)    # wrap
 
+    constant = cv2.copyMakeBorder(img, top_size, bottom_size, left_size, right_size, borderType=cv2.BORDER_CONSTANT, value=0)    # 常量法 常数填充
 
+    import matplotlib.pyplot as plt
 
+    plt.subplot(231), plt.imshow(img, 'gray'), plt.title('original')
+    plt.subplot(232), plt.imshow(replicate, 'gray'), plt.title('BORDER_REPLICATE')
+    plt.subplot(233), plt.imshow(reflect, 'gray'), plt.title('BORDER_REFLECT')
+    plt.subplot(234), plt.imshow(reflect101, 'gray'), plt.title('BORDER_REFLECT_101')
+    plt.subplot(235), plt.imshow(wrap, 'gray'), plt.title('BORDER_WRAP')
+    plt.subplot(236), plt.imshow(constant, 'gray'), plt.title('BORDER_CONSTANT')
+    plt.show()
 
-
-
+# part 8 数值计算
+def image_compute():
+    img_cat = cv2.imread("../data/cat.jpg") 
+    img_dog = cv2.imread("../data/dog.jpg") 
+    
+    print('\n\nimg_cat\n', img_cat[:5, :, 0])
+    img_cat2 = img_cat + 10         
+    print('img_cat2\n', img_cat2[:5, :, 0])     # 每个位置都加10
+    
+    # 图像加法：维度相同的图像才能加减运算，每个像素点[0, 255], 越界后会自动 % 256
+    print("sum(img_cat, img_cat2): \n", (img_cat + img_cat2)[:5, :, 0])     # 两个图像的像素值相加，等于对256取余(%256). 相加 < 256, 是和；相加 > 256 像素值超了%256
+    print("cv2.add: ", cv2.add(img_cat, img_cat))                           # 相加 > 255, 取最大值255；相加 < 256, 取相加的和
+    
+    cvshow("sum(img_cat, imgcat2): ", img_cat + img_cat2)
+    cvshow("cv2.add(img_cat, imgcat2): ", cv2.add(img_cat, img_cat2))       
+    
+    # 图像融合，叠加
+    if img_cat.shape == img_dog.shape:
+        # 通过res = α * x1 + β * x2 + b,  α,β是猫和狗两幅图像的权重系数
+        res = cv2.addWeighted(img_cat, 0.5, img_dog, 0.5, 10)
+        cvshow("img_cat-img_dog ronghe", res)
+    else:
+        img_cat = cv2.resize(img_cat, (400, 300))
+        img_dog = cv2.resize(img_dog, (400, 300))
+        res = cv2.addWeighted(img_cat, 0.5, img_dog, 0.5, 10)       # α * x1 + β * x2 + b
+        cvshow("img_cat-img_dog ronghe", res)
+        
+    print(img_cat.shape)
+    print(img_dog.shape)
+    
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    padding()     # 
+    image_compute()     # 图像数值计算、两个图像按比例融合
+    
